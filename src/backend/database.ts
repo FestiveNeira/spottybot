a8754960cedfd68e7516b8768083350447953b68
Jasper Rutherford <jasperrutherford874@gmail.com>
2025-04-01 12:22:43 -0500

    refactored database factory

--- src/backend/database.ts
+++
@@ -1,9 +0,0 @@
-import { DatabaseService } from '../database/databaseservice.js';
-
-// Not sure what path will be yet
-const sqlitepath = 'path/to/database.sqlite';
-
-// Create a database service instance (SQLite or PostgreSQL) 
-const dbService = new DatabaseService(false, sqlitepath); // false for SQLite
-
-// Backend code related to talking to the database goes here
